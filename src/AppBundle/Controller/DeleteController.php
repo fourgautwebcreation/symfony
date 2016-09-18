619c95e90eb3c6d60ed8d6d1679c1b662467bcf6
fourgautwebcreation <yann.fourgaut@hotmail.fr>
2016-09-18 17:51:44 +0200

    Mise à jour suite tests

---
+++ src/AppBundle/Controller/DeleteController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use AppBundle\Entity\Enterprises;
+
+class DeleteController extends Controller
+{
+ /**
+ *@Route("/admin/delete", name="delete")
+ */
+
+ public function indexAction(Request $request)
+ {
+  if ($request->getMethod() == 'POST') {
+      if($this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')){
+       $datas = $_POST['delete_enterprise'];
+       $em = $this->getDoctrine()->getManager();
+       $enterprise = $em->getRepository('AppBundle:Enterprises')->find($datas['id']);
+       $em->remove($enterprise);
+       $em->flush();
+       //redirection
+       return $this->redirectToRoute('admin',array(),301);
+      }
+  }
+ }
+}
---
+++ src/AppBundle/Form/UpdateForm.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use AppBundle\Forms\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+class UpdateForm extends AbstractType
+{
+
+ public function configureOptions(OptionsResolver $resolver)
+    {
+    $resolver
+         ->setDefaults(array(
+         'data_class' => 'AppBundle\Entity\Enterprises',
+         'select' => null,
+         'id' => null
+         ))
+        ->setRequired(array('select','type','id'))
+        ->setAllowedTypes('select','array')
+        ->setAllowedTypes('type','string')
+        ->setAllowedTypes('id','int')
+        ;
+    }
+
+ public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+     $select = $options['select'];
+     $id = $options['id'];
+     $type = $options['type'];
+
+     $builder->add('EnterpriseName', TextType::class,
+           array(
+            'label' => 'Nom',
+            'attr' => array('name' => 'nom'),
+            'required' => true
+               )
+           );
+     $builder->add('EnterpriseAdress', TextType::class,
+           array(
+           'label' => 'Adresse',
+           'required' => true
+              )
+           );
+     $builder->add('EnterpriseSiren', TextType::class,
+           array(
+            'label'=>'N° Siren',
+            'attr' => array('minlength' => '9'),
+            'required' => true
+               )
+           );
+           $builder->add('EnterpriseSector', ChoiceType::class,
+                 array(
+                  'choices' => $select,
+                  'label' => 'Secteur d\'activité',
+                  'required' => true
+                     )
+                 );
+     $builder->add('type', HiddenType::class, array('data' => $type, 'mapped'=>false));
+     $builder->add('id', HiddenType::class, array('data' => $id ));
+    }
+
+}
